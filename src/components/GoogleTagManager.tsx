9e6c60f999644af6eaa8231eed24c8478f75539a
Keshav Kumar <14369793+keshavkumar-in@users.noreply.github.com>
2025-03-26 18:44:59 +0530

    Update GoogleTagManager.tsx

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -13,12 +13,12 @@ export default function GoogleTagManager() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
+          })(window,document,'script','dataLayer','GTM-W856JLMV');
         `}
       </Script>
       <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          src={`https://www.googletagmanager.com/ns.html?id=GTM-W856JLMV`}
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
