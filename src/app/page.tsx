7c6b9b880e9e9b55e4d47c19685e439949a7b782
Keshav Kumar <14369793+keshavkumar-in@users.noreply.github.com>
2025-06-09 19:12:35 +0530

    fix: linting error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import PortfolioSection from "@/components/PortfolioSection.";
 import SkillsSection from "@/components/SkillsSection";
 import TimelineSection from "@/components/TimelineSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
-import ContactSection from "@/components/ContactSection";
+// import ContactSection from "@/components/ContactSection";
 import BlogSection from "@/components/BlogSection";
 import FreelanceSection from "@/components/FreelanceSection";
 import FooterSection from "@/components/FooterSection";
